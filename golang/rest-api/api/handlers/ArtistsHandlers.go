2fe13b53fea96f74c88e42a72ddd6699e005b6d8
emanuelvsz <usermane13@gmail.com>
2023-02-16 18:17:44 -0300

    creating list one patient route

--- golang/rest-api/api/handlers/ArtistsHandlers.go
+++ golang/rest-api/api/handlers/ArtistsHandlers.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"fmt"
 	"main/utils/data"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,4 +13,23 @@ func ListArtists(c echo.Context) error {
 	listArtist := data.Artists
 
 	return c.JSON(http.StatusOK, listArtist)
+}
+
+func ListArtistByID(c echo.Context) error {
+	id := c.QueryParam("artist/:artistID")
+
+	artistID, err := strconv.Atoi(id)
+	if err != nil {
+		return http.ErrAbortHandler
+	}
+
+	for i := 0; i < len(data.Artists); i++ {
+		if data.Artists[i].ID == artistID {
+			artist := data.Artists[i]
+			fmt.Println("Eoias")
+			return c.JSON(http.StatusOK, artist)
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, nil)
 }
--- golang/rest-api/main.go
+++ golang/rest-api/main.go
@@ -13,9 +13,14 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/artists", handlers.ListArtists)
+	e.GET("/artist", handlers.ListArtists)
+	e.GET("/artist/:artistID", handlers.ListArtistByID)
 	e.GET("/songs", handlers.ListSongs)
 	e.GET("/genres", handlers.ListGenres)
 
 	e.Logger.Fatal(e.Start(":8081"))
+}
+
+func artistsRoutes() {
+
 }
